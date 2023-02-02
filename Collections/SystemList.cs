e8805e59cbab123147416030b07c5124bb8681a1
Lance Crisang <lancecrisang14@gmail.com>
2023-02-02 09:08:57 +0800

    Finish base implementation

--- Collections/SystemList.cs
+++ Collections/SystemList.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MiniEngine.Collections
 {
@@ -9,18 +10,50 @@ namespace MiniEngine.Collections
         public SystemNode? Next { get; set; }
 
         public System? Value { get; set; }
+
+        public void InsertBefore(SystemNode node)
+        {
+            var prev = Previous;
+            if (prev == null)
+            {
+                Previous = node;
+                return;
+            }
+
+            prev.Next = node;
+            node.Previous = prev;
+            node.Next = this;
+            Previous = node;
+        }
+
+        public void InsertAfter(SystemNode node)
+        {
+            var next = Next;
+            if (next == null)
+            {
+                Next = node;
+                return;
+            }
+
+            next.Previous = node;
+            node.Next = next;
+            node.Previous = this;
+            Next = node;
+
+        }
         
         public static implicit operator System(SystemNode node) => node.Value!;
     }
+
     public class SystemListEnumerator : IEnumerator<System>
     {
-        private SystemNode? _current;
-        private SystemNode? _head {get; init;}
-        private bool disposedValue;
+        protected SystemNode? _current;
+        protected SystemNode? _head;
+        private bool _disposedValue;
 
         public SystemListEnumerator(SystemNode? head)
         {
-            _current = head;
+            _current = null;
             _head = head;
         }
 
@@ -30,10 +63,17 @@ namespace MiniEngine.Collections
 
         bool IEnumerator.MoveNext()
         {
-            if (_current?.Next != null)
-                return false;
-            _current = _current!.Next!;
-            return true;
+            switch (_current)
+            {
+                case { Next: null }:
+                    return false;
+                case null:
+                    _current = _head;
+                    return true;
+                default:
+                    _current = _current!.Next!;
+                    return true;
+            }
         }
 
         void IEnumerator.Reset()
@@ -43,34 +83,73 @@ namespace MiniEngine.Collections
 
         protected virtual void Dispose(bool disposing)
         {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects)
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
-            }
+            if (_disposedValue)
+                return;
+            _current = null;
+            _head = null;
+            _disposedValue = true;
         }
-
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~SystemListEnumerator()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
 
         void global::System.IDisposable.Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
             global::System.GC.SuppressFinalize(this);
         }
     }
-    public class SystemList : IEnumerable<System>
+
+    public class SystemListNodeEnumerator : IEnumerator<SystemNode>
+    {
+        private SystemNode? _current;
+        private SystemNode? _head;
+        private bool _disposedValue;
+
+        public SystemListNodeEnumerator(SystemNode? head)
+        {
+            _current = null;
+            _head = head;
+        }
+
+        SystemNode IEnumerator<SystemNode>.Current => _current!;
+
+        object IEnumerator.Current => _current!;
+
+        bool IEnumerator.MoveNext()
+        {
+            switch (_current)
+            {
+                case { Next: null }:
+                    return false;
+                case null:
+                    _current = _head;
+                    return true;
+                default:
+                    _current = _current!.Next!;
+                    return true;
+            }
+        }
+
+        void IEnumerator.Reset()
+        {
+            _current = _head;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposedValue)
+                return;
+            _current = null;
+            _head = null;
+            _disposedValue = true;
+        }
+
+        void global::System.IDisposable.Dispose()
+        {
+            Dispose(disposing: true);
+            global::System.GC.SuppressFinalize(this);
+        }
+    }
+
+    public class SystemList : IEnumerable<System>, IEnumerable<SystemNode>
     {
         private SystemNode? _head = null;
 
@@ -79,9 +158,52 @@ namespace MiniEngine.Collections
             return new SystemListEnumerator(_head);
         }
 
+        IEnumerator<SystemNode> IEnumerable<SystemNode>.GetEnumerator()
+        {
+            return new SystemListNodeEnumerator(_head);
+        }
+
         IEnumerator IEnumerable.GetEnumerator()
         {
             return new SystemListEnumerator(_head);
+        }
+
+        public bool ContainsSystemType<T>() where T : System
+        {
+            return FindSystemBySystemType<T>() != null;
+        }
+
+        public System? FindSystemBySystemType<T>() where T : System
+        {
+            var type = typeof(T);
+            return this.FirstOrDefault<System>(system => system.GetType() == type);
+        }
+
+        public SystemNode? FindSystemNodeBySystemType<T>() where T : System
+        {
+            var type = typeof(T);
+            return this.FirstOrDefault<SystemNode>(system => system?.Value?.GetType() == type);
+        }
+
+        public void Add(System system)
+        {
+            if (_head is null)
+            {
+                _head = new SystemNode
+                {
+                    Value = system
+                };
+                return;
+            }
+            var lastNode = _head;
+            while (lastNode.Next != null)
+                lastNode = lastNode.Next;
+            var newNode = new SystemNode
+            {
+                Value = system,
+                Previous = lastNode
+            };
+            lastNode.Next = newNode;
         }
     }
 }
--- Services/SystemService.cs
+++ Services/SystemService.cs
@@ -9,74 +9,44 @@ namespace MiniEngine
 {
     public static class SystemService
     {
-        private static readonly List<System> _systems = new();
         private static readonly SystemList _systemList = new();
-        private static readonly Dictionary<Type, (System, MethodInfo)> _associatedHandlers = new();
 
         internal static void LoadSystems()
         {
-            if (_associatedHandlers.Any())
-                return;
-
-            // load systems components via reflection
-            var assembly = Assembly.GetExecutingAssembly();
-            var types = assembly.GetTypes();
-
-            var systems = new List<Type>();
-            var components = new List<Type>();
-            foreach (var type in types)
-            {
-                if (type.IsAssignableTo(typeof(System)) &&
-                    type != typeof(System))
-                    systems.Add(type);
-                if (type.IsAssignableTo(typeof(Component)) &&
-                    type != typeof(Component))
-                    components.Add(type);
-            }
-
-            systems.ForEach(HandleSystem);
-            LoggingService.Debug("Processed {0} system(s).", systems.Count);
-            components.ForEach(HandleComponent);
-            LoggingService.Debug("Processed {0} component(s).", components.Count);
         }
 
-        private static void HandleComponent(Type type)
+        public static void RegisterBefore<T>(System system) where T : System
         {
-            var attributes = type.GetCustomAttributes();
-            foreach (var attribute in attributes)
+            if (typeof(T) == typeof(void))
             {
-                if (!attribute.GetType().IsOfGenericType(typeof(HandledByAttribute<>)))
-                    continue;
-                var systemType = ((IHandledByAttribute)attribute).SystemType;
-                var systemInstance = _systems.First(system => system.GetType() == systemType);
-                var systemMethods = systemType.GetMethods();
-                var method = systemMethods.FirstOrDefault(methodInfo =>
-                {
-                    if (methodInfo is null ||
-                        methodInfo.ReturnType != typeof(void) ||
-                        methodInfo.GetParameters().Length != 1 ||
-                        methodInfo.Name != "HandleComponent")
-                        return false;
-                    var firstParameter = methodInfo.GetParameters()[0];
-                    return firstParameter.ParameterType == type;
-                }, null);
-                if (method is null)
-                {
-                    LoggingService.Warn("{0} is handled by {1} but does not have a method that matches \"{2}\".",
-                        type,
-                        systemType,
-                        $"void HandleComponent({type})");
-                    continue;
-                }
-                _associatedHandlers.Add(type, (systemInstance, method));
+                _systemList.Add(system);
             }
+            _systemList.FindSystemNodeBySystemType<T>()?.InsertBefore(new SystemNode
+            {
+                Value = system
+            });
         }
 
-        private static void HandleSystem(Type type)
+        public static void RegisterAfter<T>(System system) where T : System
         {
-            var system = (System?)type.GetConstructor(Array.Empty<Type>())?.Invoke(null);
-            if (system != null)
-                _systems.Add(system);
+            if (typeof(T) == typeof(void))
+            {
+                _systemList.Add(system);
+            }
+            _systemList.FindSystemNodeBySystemType<T>()?.InsertAfter(new SystemNode
+            {
+                Value = system
+            });
+        }
+
+        public static void RegisterComponentInstance(Component component)
+        {
+
+        }
+
+        public static void PurgeComponents()
+        {
+
         }
     }
 }
