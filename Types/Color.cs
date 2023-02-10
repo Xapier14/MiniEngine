184310f61ee6882902508fb34b7e427aacbe8149
Lance Crisang <lancecrisang14@gmail.com>
2023-02-10 12:34:34 +0800

    Add color struct

---
+++ Types/Color.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEngine
+{
+    public struct Color
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+        public byte A { get; set; }
+
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = 255;
+        }
+
+        public Color(byte r, byte g, byte b, byte a)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+        
+        public static implicit operator Color((byte r, byte g, byte b) color)
+        {
+            return new Color(color.r, color.g, color.b);
+        }
+        
+        public static implicit operator Color((byte r, byte g, byte b, byte a) color)
+        {
+            return new Color(color.r, color.g, color.b, color.a);
+        }
+        
+        public static implicit operator Color(string hex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
